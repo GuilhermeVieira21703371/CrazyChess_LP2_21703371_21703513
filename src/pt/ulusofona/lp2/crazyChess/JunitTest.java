8ce0590d142c2fb0fcb77551ef56795a57330df9
a21703513 <xiaoyuexie@hotmail.com>
2018-11-30 14:27:16 +0000

    test

---
+++ src/pt/ulusofona/lp2/crazyChess/JunitTest.java
@@ -0,0 +1,19 @@
+package pt.ulusofona.lp2.crazyChess;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class JunitTest {
+
+	@Test
+	public void test1 (){
+		Simulador test = new Simulador();
+		test.tamanhoTabuleiro = 8;
+		test.tabuleiro = new int[test.tamanhoTabuleiro][test.tamanhoTabuleiro];
+
+
+
+	}
+
+
+}
