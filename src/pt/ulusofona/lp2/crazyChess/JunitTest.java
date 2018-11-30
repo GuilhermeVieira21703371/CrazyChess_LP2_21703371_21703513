f985b528ffb88ee0e7eda1c0bba82913c43484e7
a21703513 <xiaoyuexie@hotmail.com>
2018-11-30 15:56:51 +0000

    test

--- src/pt/ulusofona/lp2/crazyChess/JunitTest.java
+++ src/pt/ulusofona/lp2/crazyChess/JunitTest.java
@@ -3,16 +3,23 @@ package pt.ulusofona.lp2.crazyChess;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class JunitTest {
+public class JunitTest {
+
 
 	@Test
-	public void test1 (){
-		Simulador test = new Simulador();
-		test.tamanhoTabuleiro = 8;
-		test.tabuleiro = new int[test.tamanhoTabuleiro][test.tamanhoTabuleiro];
+	public void test1Alcunha(){
+		CrazyPiece peca1 = new CrazyPiece(2,0,0,"Chefe");
+		String resultadoEsperado = "Selvagem";
+		String resultadoObtido = peca1.alcunha;
+		assertEquals(resultadoEsperado,resultadoObtido);
+	}
 
-
-
+	@Test
+	public void test2idPeca(){
+		CrazyPiece peca2 = new CrazyPiece(1,0,0,"Chefe");
+		int resultadoEsperado = 1;
+		int resultadoObtido = peca2.idPeca;
+		assertEquals(resultadoEsperado,resultadoObtido);
 	}
 
 
